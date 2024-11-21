27bb7f46d7b99afcb320e6354c64bf792c24f62f
Ashwin Krishna K <ashwinkrishna7151@gmail.com>
2024-11-21 20:12:04 +0530

    Examples for creating modules, importing modules from custom packages and subpackages

---
+++ module/package/__init__.py
@@ -0,0 +1,3 @@
+"""__init__.py file indicates that the directory should be treated as a Python package.
+This was necessary in Python 2 but is optional in Python 3.
+ However, including an __init__.py file is still considered good practice."""
